bd2da32100d3e180ac755d38b6778abaeab4c022
Miguel Santamaria <msantamaria@hugeinc.com>
2015-11-04 09:04:46 -0500

    capturing requested url

--- public/server.js
+++ public/server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var url = require('url');
 
 function start() {
 	function onRequest(request, response) {
-		console.log('request received');
+		var pathname = url.parse(request.url).pathname;
+		console.log('request for: ' + pathname + ' received.');
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.write('hello world');
 		response.end();
