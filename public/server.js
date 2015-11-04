a9b7317a13aaf2448752ef3353e70dc13d08af9e
Miguel Santamaria <msantamaria@hugeinc.com>
2015-11-03 19:19:41 -0500

    module for server.js, using index.js to wrap the app

--- public/server.js
+++ public/server.js
@@ -1,7 +1,15 @@
 var http = require('http');
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('hello world');
-	response.end();
-}).listen(8888);
+function start() {
+	function onRequest(request, response) {
+		console.log('request received');
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write('hello world');
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log('Serving on port 8888');
+}
+
+exports.start = start;
