6aa506ff4ef6b870fbe086e26f3f1f47b3bf7169
Miguel Angel Santamaría <santamaria.migue@gmail.com>
2015-11-11 00:19:15 -0500

    Added requestHandler file and modified the flow of the responses

--- public/server.js
+++ public/server.js
@@ -1,13 +1,12 @@
 var http = require('http');
 var url = require('url');
 
-function start() {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log('request for: ' + pathname + ' received.');
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write('hello world');
-		response.end();
+
+		route(handle, pathname, response);
 	}
 
 	http.createServer(onRequest).listen(8888);
